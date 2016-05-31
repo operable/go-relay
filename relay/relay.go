fa3eee3a22ed2ac7c8e1842086af7277dbe62a84
Kevin Smith <kevin@operable.io>
2016-05-31 11:49:45 -0400

    Allow start up if only native engine is enabled

--- relay/relay.go
+++ relay/relay.go
@@ -66,12 +66,14 @@ func NewRelay(config *config.Config) (Relay, error) {
 }
 
 func (r *cogRelay) Start() error {
-	dockerEngine, err := r.engines.GetEngine(engines.DockerEngineType)
-	if err != nil {
-		return err
-	}
-	if err := dockerEngine.Init(); err != nil {
-		return err
+	if r.config.DockerEnabled() == true {
+		dockerEngine, err := r.engines.GetEngine(engines.DockerEngineType)
+		if err != nil {
+			return err
+		}
+		if err := dockerEngine.Init(); err != nil {
+			return err
+		}
 	}
 	r.connOpts = bus.ConnectionOptions{
 		Userid:        r.config.ID,
