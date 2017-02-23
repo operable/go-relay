56e1bf83923abc7262bee7b296fae7f37a3160f1
Christopher Maier <chris@operable.io>
2017-02-23 09:33:37 -0500

    Tweak tag detection logic

--- scripts/build_tag.sh
+++ scripts/build_tag.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-# git describe --tags appends SHA info to
-# prior tag. This subcommand detects when that
-# happens.
-tag=`git describe --tags`
-dash_count=`git describe --tags | grep -o - | wc -l | sed "s/ //g"`
-
-if [ "$dash_count" != "0" ]; then
-  tag="dev"
+# Writes the current tag to standard out. If the current commit isn't
+# tagged then the current branch name is used.
+tag=`git describe --tags --exact-match`
+if [ "$?" != 0 ]
+then
+    tag=$(git rev-parse --abbrev-ref HEAD)
 fi
 
 echo $tag
