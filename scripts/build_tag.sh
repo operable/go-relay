75d7154a85c3926032eea74452a19d9b12731826
Christopher Maier <christopher.maier@gmail.com>
2017-03-12 21:06:18 -0400

    Swallow STDERR output when resolving current tag

--- scripts/build_tag.sh
+++ scripts/build_tag.sh
@@ -2,7 +2,7 @@
 
 # Writes the current tag to standard out. If the current commit isn't
 # tagged then the current branch name is used.
-tag=`git describe --tags --exact-match`
+tag=`git describe --tags --exact-match 2>/dev/null`
 if [ "$?" != 0 ]
 then
     tag=$(git rev-parse --abbrev-ref HEAD)
